bea5ef4ec1faa5804ebcc4f064052820cdc36bb0
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-28 09:21:47 -0800

    fix dog + coffee interaction

--- scenes/coffee.cs
+++ scenes/coffee.cs
@@ -6,12 +6,12 @@ public partial class coffee : StaticBody2D
 
 	public override void _Ready()
 	{
-		gdScriptNode = GetNode("../Player");
+		gdScriptNode = GetNode("../player");
 	}
 	public Node gdScriptNode;
 	private void _on_drank_body_entered(Node2D body)
 	{
-		if (body.Name == "Player")
+		if (body.Name == "player")
 		{
 			gdScriptNode.Call("set_speed", 450);
 			QueueFree();
