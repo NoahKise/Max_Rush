8bf9182b472f36a0970f0a009ba043e0a3f40195
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-28 13:07:06 -0800

    coffee tweaks

--- scenes/CoffeePath.cs
+++ scenes/CoffeePath.cs
@@ -3,18 +3,38 @@ using System;
 
 public partial class CoffeePath : Sprite2D
 {
-	public start postion;
+	private Vector2 StartPosition;
 	private PathFollow2D pathFollow;
 	[Export]
-	public float speed = 25f;
+	public float speed = 15f;
+	public int ticker;
 
 	public override void _Ready()
 	{
 		pathFollow = GetParent<PathFollow2D>();
+		StartPosition = GlobalPosition;
+		ticker = 1;
+		//GD.Print(StartPosition);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		pathFollow.HOffset += (float)(speed * delta);
+		if (ticker % 2 != 0 )
+		{
+			pathFollow.HOffset += (float)(speed * delta);
+		}
+		else 
+		{
+			pathFollow.HOffset -= (float)(speed * delta);
+		}
+		
+		if (pathFollow.HOffset == 5 || pathFollow.HOffset == -5)
+		{
+			ticker++;
+		}
+		
+		
+		
+		GD.Print(pathFollow.HOffset);
 	}
 }
